f6065b4c36aa44f765e2d3f6005139a615d6484f
Prasanna <psprasanna3011@gmail.com>
2025-11-23 14:28:13 +0530

    Added route for vercel

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,20 +5,22 @@ import CTA from "../components/CTA";
 
 const Projects = () => {
     return (
-        <section className="max-container">
+        <section className="max-container mt-2">
             <h1 className="head-text">
                 My <span className="blue-gradient_text font-semibold drop-shadow">Projects</span>
             </h1>
 
-            <div className="mt-5 flex flex-col gap-3 text-slate-500">
-                <p>I've embarked on numerous projects throughout the years, but these are
+            <div className="mt-3 flex flex-col gap-3 text-slate-500">
+                <p>
+                    Throughout my development journey, I’ve worked on projects that challenged me, taught me, and helped me grow. The projects highlighted here are especially close to my heart and reflect the passion I bring to everything I build.
+                    {/* I've embarked on numerous projects throughout the years, but these are
                     the ones I hold closest to my heart. Many of them are open-source, so if
                     you come across something that piques your interest, feel free to
                     explore the codebase and contribute your ideas for further enhancements.
-                    Your collaboration is highly valued!
+                    Your collaboration is highly valued! */}
                 </p>
 
-                <div className="flex flex-wrap my-20 gap-16">
+                <div className="flex flex-wrap mt-20 mb-10 gap-16">
                     {projects.map((project) => (
                         <div className="lg:w-[400px] w-full" key={project.name}>
                             <div className="block-container w-12 h-12">
